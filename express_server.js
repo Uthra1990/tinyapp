ae04c1b35a402f89cb57247b28cc046fbbbf262a
Uthra1990 <uthraravichandran29@gmail.com>
2021-09-13 15:08:31 -0400

    Delete functionality added

--- express_server.js
+++ express_server.js
@@ -49,6 +49,12 @@ app.get("/urls.json", (req, res) => {
       res.redirect(longURL);
   });
 
+  app.post("/urls/:shortURL/delete", (req, res) => {
+    const shortURL = req.params.shortURL;
+    delete urlDatabase[shortURL];
+    res.redirect('/urls');
+  });
+
   app.post("/urls", (req, res) => {
   const longURL = req.body.longURL
   const shortURL = generateRandomString() 
